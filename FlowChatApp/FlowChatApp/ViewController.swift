a53813aa2f10a92aa6608035cc2f4ad71ad80a9d
ŞEVVAL ATEŞ <116071942+SevvalAtes54@users.noreply.github.com>
2023-10-06 19:33:13 +0300

    Add files via upload

---
+++ FlowChatApp/FlowChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlowChatApp
+//
+//  Created by Macbook Air on 5.10.2023.
+//
+
+import UIKit
+import TinyConstraints
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
